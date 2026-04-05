1d5a1743ee57afbb46c2fc943f2e3f5aa55a9ab5
HappyToPig <3304661481@qq.com>
2026-04-05 22:10:42 +0800

    封装验证码工具类

---
+++ smart-learning-academy-common/src/main/java/com/shxy/smartlearningacademycommon/exception/SmsVerifyCodeSendFailException.java
@@ -0,0 +1,13 @@
+package com.shxy.smartlearningacademycommon.exception;
+
+/**
+ * @author Wu, Hui Ming
+ * @version 1.0
+ * @School Suihua University
+ * @since 2026/4/5 22:00
+ */
+public class SmsVerifyCodeSendFailException extends RuntimeException {
+    public SmsVerifyCodeSendFailException(String message) {
+        super("发送短信验证码失败：" + message);
+    }
+}
