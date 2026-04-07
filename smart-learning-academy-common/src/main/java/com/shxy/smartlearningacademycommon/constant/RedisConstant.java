541e8afb39875be117f948b174082d2402ee144f
HappyToPig <3304661481@qq.com>
2026-04-07 16:28:20 +0800

    完善登录注册接口

---
+++ smart-learning-academy-common/src/main/java/com/shxy/smartlearningacademycommon/constant/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.shxy.smartlearningacademycommon.constant;
+
+/**
+ * @author Wu, Hui Ming
+ * @version 1.0
+ * @School Suihua University
+ * @since 2026/4/7 15:52
+ */
+public class RedisConstant {
+    public static final String USER_TOKEN_KEY_PREFIX = "user:token:";
+    public static final String USER_INFO_KEY_PREFIX="user:info:";
+}
