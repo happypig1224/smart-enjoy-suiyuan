9a0ca4e0c2f97ff51e1ed51274081d6b6b8dd000
Happy pig <16246754+happytopig@user.noreply.gitee.com>
2026-04-27 21:03:21 +0800

    修复bug

---
+++ smart-enjoy-suiyuan-server/src/main/java/com/shxy/suiyuanserver/config/MyMetaObjectHandler.java
@@ -0,0 +1,33 @@
+package com.shxy.suiyuanserver.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    /**
+     * 插入时自动填充
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.debug("开始插入填充...");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    /**
+     * 更新时自动填充
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.debug("开始更新填充...");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
